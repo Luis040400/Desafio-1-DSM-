d9a4574431585a704a328fcc1d36a7d7029b1cb4
Luis040400 <cheleluis04@gmail.com>
2023-02-26 18:40:18 -0600

    Fix an error

--- app/src/main/java/com/example/desafiopractico/Ejercicio2.kt
+++ app/src/main/java/com/example/desafiopractico/Ejercicio2.kt
@@ -3,6 +3,7 @@ package com.example.desafiopractico
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -36,7 +37,9 @@ class Ejercicio2 : AppCompatActivity() {
             val a = txtA.text.toString().toDouble()
             val b = txtB.text.toString().toDouble()
             val c = txtC.text.toString().toDouble()
-
+            Log.d("VARIBLE A",a.toString())
+            Log.d("VARIABLE B",b.toString())
+            Log.d("VARIABLE C",c.toString())
             val discriminante = b * b - (4 * a * c)
             if(discriminante>0){
                 val x1 = (-b + sqrt(discriminante))/(2*a)
@@ -44,7 +47,7 @@ class Ejercicio2 : AppCompatActivity() {
                 tvX1.setText("Solución X1: " + x1)
                 tvX2.setText("Solución X2: " + x2)
             }
-            else if(discriminante>0){
+            else if(discriminante == 0.0){
                 val x = -b / (2*a)
                 tvX1.setText("Solución X1: " + x)
                 Toast.makeText(this, "Tiene una sola solución",
