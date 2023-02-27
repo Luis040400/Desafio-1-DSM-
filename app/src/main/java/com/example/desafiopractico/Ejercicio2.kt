6e4e65307cc80589651dcb6b4d82339263ef95f1
Luis040400 <cheleluis04@gmail.com>
2023-02-26 20:22:01 -0600

    Arreglando algunos diseños

--- app/src/main/java/com/example/desafiopractico/Ejercicio2.kt
+++ app/src/main/java/com/example/desafiopractico/Ejercicio2.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -33,26 +34,31 @@ class Ejercicio2 : AppCompatActivity() {
         btnEj2Volver.setOnClickListener{goToMenu()}
     }
     fun calculate(){
+        tvX1.visibility = View.GONE
+        tvX2.visibility = View.GONE
         if (txtA.text.toString().isNotEmpty() && txtB.text.toString().isNotEmpty() && txtC.text.toString().isNotEmpty()){
             val a = txtA.text.toString().toDouble()
             val b = txtB.text.toString().toDouble()
             val c = txtC.text.toString().toDouble()
-            Log.d("VARIBLE A",a.toString())
-            Log.d("VARIABLE B",b.toString())
-            Log.d("VARIABLE C",c.toString())
             val discriminante = b * b - (4 * a * c)
             if(discriminante>0){
+                clear()
                 val x1 = (-b + sqrt(discriminante))/(2*a)
                 val x2 = (-b - sqrt(discriminante))/(2*a)
+                tvX1.visibility = View.VISIBLE
+                tvX2.visibility = View.VISIBLE
                 tvX1.setText("Solución X1: " + x1)
                 tvX2.setText("Solución X2: " + x2)
             }
             else if(discriminante == 0.0){
+                clear()
                 val x = -b / (2*a)
+                tvX1.visibility = View.VISIBLE
                 tvX1.setText("Solución X1: " + x)
                 Toast.makeText(this, "Tiene una sola solución",
                     Toast.LENGTH_SHORT).show()
             }else{
+                clear()
                 Toast.makeText(this, "No tiene soluciones reales",
                     Toast.LENGTH_SHORT).show()
             }
@@ -61,6 +67,10 @@ class Ejercicio2 : AppCompatActivity() {
                 Toast.LENGTH_SHORT).show()
         }
 
+    }
+    fun clear(){
+        tvX1.setText("")
+        tvX2.setText("")
     }
     fun goToMenu(){
         onBackPressed()
